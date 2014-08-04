dd7d1b0b32ede9194f3aff91b6ebe7caf10371b6
Keloran <keloran@nordicarts.net>
2014-08-04 18:11:20 +0100

    added singleton

---
+++ Tools/OS/Singleton/Singleton.hpp
@@ -0,0 +1,33 @@
+#ifndef NORDICARTS_OS_SINGLETON_H
+#define NORDICARTS_OS_SINGLETON_H
+
+#include "../OSLoad.hpp"
+
+namespace NordicArts {
+	class Singleton {
+		// Variables
+		public:
+			enum SingletonType {
+				TYPE_LOGGER = 0,
+				TYPE_SETTINGS,
+				TYPE_ENGINE,
+				TYPE_SHADER_MANAGER,
+			};
+
+		protected:
+		private:
+			SingletonType m_eType;
+
+		// Methods
+		public:
+			virtual ~Singleton();
+
+		protected:
+			explicit Singleton(SingletonType eType);
+
+		private:
+
+	};	
+};
+
+#endif
